cca0752cc7d183d08eebbf71b5673f1516b005cf
David Obregon <david.obregon@contractor.jupiterone.com>
2026-02-12 11:53:35 -0500

    Adding transparency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,32 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      flavour = "macchiato", -- latte, frappe, macchiato, mocha
-      background = { -- :h background
-        light = "latte",
-        dark = "macchiato",
-      },
-    })
-    vim.cmd.colorscheme "catppuccin"
-  end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = "macchiato", -- latte, frappe, macchiato, mocha
+			background = { -- :h background
+				light = "latte",
+				dark = "macchiato",
+			},
+			transparent_background = true, -- Enable transparency
+			show_end_of_buffer = false, -- Don't show '~' after buffer end
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				mini = {
+					enabled = true,
+					indentscope_color = "",
+				},
+			},
+			custom_highlights = function(colors)
+				return {
+					LineNr = { fg = colors.peach }, -- Change line number color
+					CursorLineNr = { fg = colors.yellow }, -- Change current line number color
+					-- Number = { fg = colors.red }, -- This is for numeric literals in code
+				}
+			end,
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
